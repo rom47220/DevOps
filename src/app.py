ac48cf62f325103b5d57d7b7ad49467c300b4e46
Astradar <lucas.dumoulin.iphone@gmail.com>
2026-09-11 14:16:09 +0200

    feat(app): add subtract function

--- src/app.py
+++ src/app.py
@@ -5,5 +5,10 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
+def subtract(a: int, b: int) -> int:
+    """Return a minus b."""
+    return a - b
+
+
 def greet(name: str) -> str:
     return f"Hello, {name}!"
